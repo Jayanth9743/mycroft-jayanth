aa6baa348922b91df0812bd5a85921d0428801b8
Jayanth P <jayanthjayram6968@gmail.com>
2024-11-22 15:11:33 +0530

    live style done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,25 +2,25 @@ import { FiUpload } from "react-icons/fi";
 
 const Form = () => {
   return (
-    <div className="flex justify-center w-1/2 h-full items-center gap-6 rounded-lg flex-col bg-[#ebebeb]">
+    <div className="flex justify-center w-1/2 h-full items-center gap-6 rounded-lg flex-col bg-[#efefef]">
 
       <div className=" flex justify-start items-center w-3/4">
-        <button className="w-24 h-8 bg-black text-white rounded-md">load config</button>
+        <button className="w-[9rem] h-[2.5rem] bg-black text-white rounded-xl">load config</button>
       </div>
 
       <div className="flex justify-center flex-col gap-1 items-start w-3/4">
         <label className="font-semibold" htmlFor="config-name">Config name</label>
-        <input className="w-full p-1 outline-none bg-white rounded" type="text" id="config-name" name="config-name" />
+        <input className="w-full p-2 outline-none bg-white rounded" type="text" id="config-name" name="config-name" />
       </div>
 
       <div className="flex justify-center flex-col gap-1 items-start w-3/4">
         <label className="font-semibold" htmlFor="bot-name">BotName</label>
-        <input className="w-full p-1 outline-none bg-white rounded" type="text" id="bot-name" name="bot-name" />
+        <input className="w-full p-2 outline-none bg-white rounded" type="text" id="bot-name" name="bot-name" />
       </div>
 
       <div className="flex justify-center flex-col gap-1 items-start w-3/4">
         <label className="font-semibold" htmlFor="font-family">Font family</label>
-        <select name="font-family" id="font-family" className="w-full p-1 outline-none bg-white rounded">
+        <select name="font-family" id="font-family" className="w-full p-2 outline-none bg-white rounded">
           <option value="">asd</option>
           <option value="">asd</option>
           <option value="">asd</option>
@@ -30,7 +30,7 @@ const Form = () => {
 
       <div className="flex justify-center flex-col gap-1 items-start w-3/4">
         <label className="font-semibold" htmlFor="header-color">Header color</label>
-        <div className="w-full flex justify-between h-8 items-center bg-white rounded">
+        <div className="w-full flex justify-between h-10 items-center bg-white rounded">
           <p className=" ml-2">sfds</p>
           <input name="header-color" id="header-color" type="color" />
         </div>
@@ -38,7 +38,7 @@ const Form = () => {
 
       <div className="flex justify-center flex-col gap-1 items-start w-3/4">
         <label className="font-semibold" htmlFor="header-font-color">Header font color</label>
-        <div className="w-full flex justify-between h-8 items-center bg-white rounded">
+        <div className="w-full flex justify-between h-10 items-center bg-white rounded">
           <p className=" ml-2">sfds</p>
           <input name="header-font-color" id="header-font-color" type="color" />
         </div>
@@ -46,7 +46,7 @@ const Form = () => {
 
       <div className="flex justify-center flex-col gap-1 items-start w-3/4">
         <label className="font-semibold" htmlFor="background-color">Background color</label>
-        <div className="w-full flex justify-between h-8 items-center bg-white rounded">
+        <div className="w-full flex justify-between h-10 items-center bg-white rounded">
           <p className=" ml-2">sfds</p>
           <input name="background-color" id="background-color" type="color" />
         </div>
@@ -54,7 +54,7 @@ const Form = () => {
 
       <div className="flex justify-center flex-col gap-1 items-start w-3/4">
         <label className="font-semibold"  htmlFor="chat-font-color">Chat font color</label>
-        <div className="w-full flex justify-between h-8 items-center bg-white rounded">
+        <div className="w-full flex justify-between h-10 items-center bg-white rounded">
           <p className="ml-2">sfds</p>
           <input name="chat-font-color" id="chat-font-color" type="color" />
         </div>
@@ -62,7 +62,7 @@ const Form = () => {
 
       <div className="flex justify-center flex-col gap-1 items-start w-3/4">
         <p className="font-semibold">Avatar image</p>
-        <label className="w-full flex h-8 justify-between items-center bg-white rounded" htmlFor="avatar-image">
+        <label className="w-full flex h-10 justify-between items-center bg-white rounded" htmlFor="avatar-image">
           <img src="" alt="" />
           <FiUpload className="text-lg cursor-pointer mr-2"/> 
         </label>
@@ -71,7 +71,7 @@ const Form = () => {
 
       <div className="flex justify-center flex-col gap-1 items-start w-3/4">
         <p className="font-semibold">Launcher image</p>
-        <label className="w-full flex h-8 justify-between items-center bg-white rounded" htmlFor="launcher-image">
+        <label className="w-full flex h-10 justify-between items-center bg-white rounded" htmlFor="launcher-image">
           <img src="" alt="" />
           <FiUpload className="text-lg cursor-pointer mr-2"/> 
         </label>
--- src/components/LivePreview.jsx
+++ src/components/LivePreview.jsx
@@ -1,9 +1,44 @@
-
+import { MdOutlineClear } from "react-icons/md";
+import img from "../assets/react.svg"
+import { IoMicOutline } from "react-icons/io5";
 
 const LivePreview = () => {
   return (
-    <div className="flex justify-center items-center w-1/2 h-full">
-        
+    <div className="flex justify-center flex-col shadow-2xl  items-center w-1/2 h-full">
+
+        <div className="flex justify-center mb-12 font-semibold items-center w-full gap-2">
+          <p className="w-2 h-2 rounded-full bg-green-500"></p>
+          <p>Live Preview</p>
+        </div>
+
+        <div className="w-3/5 h-[60%] flex flex-col justify-between shadow-left-right shadow-xl items-center">
+
+            <div className="flex justify-between items-center rounded-t-xl w-full bg-[#E6391D] h-12">
+              <p className="ml-2 text-white font-medium">Greebo</p>
+              <MdOutlineClear className="text-white mr-2"/>
+            </div>
+
+            <div className="flex w-full justify-around items-center mb-60 gap-2">
+              <img src={img} alt="" className="object-contain w-6 h-6 rounded-full" />
+              <p className="w-5/6 text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sequi a, dolorem quo culpa inventore aspernatur nam officiis.</p>
+            </div>
+
+            <div className="w-full rounded-b-xl shadow-md relative">
+              <input type="text" className="w-full outline-none rounded-b-xl p-4" placeholder="Need help just type or say it" />
+              <IoMicOutline className="absolute right-3 top-5 text-xl"/>
+            </div>
+
+        </div>
+
+        <div className="relative w-3/5 mb-12 h-12">
+          <img src={img} alt="" className="absolute top-0 right-0 object-contain w-12 h-12" />
+        </div>
+
+        <div className="flex justify-center items-center gap-2 w-full flex-col">
+          <button className="w-[10rem] h-[2.5rem] rounded-xl bg-black text-white">Download config</button>
+          <p>kdujfhsdf</p>
+        </div>
+
     </div>
   )
 }
